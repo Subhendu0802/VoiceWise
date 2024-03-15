58816fe4b630f1ae7fbbc47ea125796f29559315
Subhendu Adhikari <89163743+Subhendu0802@users.noreply.github.com>
2024-03-15 17:16:23 +0530

    Delete func/Email.py

--- func/Email.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-my_email="subhendudemo@gmail.com"
-password="ldfewiopivgqkwyw"
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()#transport layer security to secure email server
-    connection.login(user=my_email,password=password)
-    connection.sendmail(from_addr=my_email,to_addrs="ssadhikari1512@gmail.com",
-                        msg="Subject:hello\n\n Dude hii")
